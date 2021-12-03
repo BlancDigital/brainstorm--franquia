3a90495689579534d56a293c5ff93d28dca95539
joaoHass <smartpocketbr@gmail.com>
2021-12-03 18:04:18 -0300

    first commit

---
+++ enviar.php
@@ -0,0 +1,43 @@
+<?php
+
+// Passando os dados obtidos pelo formulário para as variáveis abaixo e sanitizando
+$emailremetente   = 'no-reply@blancmarketingdigital.com.br';
+$nome             = htmlspecialchars($_POST['name-form'], ENT_QUOTES);
+$email            = htmlspecialchars($_POST['email-form'], ENT_QUOTES);
+$telefone      	  = htmlspecialchars($_POST['cellphone-form'], ENT_QUOTES);
+$url              = htmlspecialchars($_POST['url-form'], ENT_QUOTES);
+$emailatt_cliente = 'contato@escolabrainstorm.com.br';
+$emailatt_leads   = 'leads@blancmarketingdigital.com.br';
+
+/* =-=-=-= Informações base do e-mail (enviada para todos os destinatários) =-=-=-=*/
+$corpoHTML = '<strong>Formulário de Contato</strong>
+
+<p><b>Nome:</b>               '.$nome.'           </p>
+<p><b>E-Mail:</b>             '.$email.'          </p>
+<p><b>Telefone:</b>           '.$telefone.'       </p>
+<hr>';
+
+// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
+// O return-path deve ser ser o mesmo e-mail do remetente.
+$headers  = "MIME-Version: 1.1\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n";
+$headers .= "From: $emailremetente\r\n"; // remetente
+/* =-=-=-= Fim das informações base do e-mail =-=-=-= */
+
+
+// Envio para o e-mail do cliente
+$header_cliente = $headers . "Return-Path: $emailatt_cliente \r\n";
+$envio_cliente  = mail($emailatt_cliente,"[Lead] Brainstorm - Escoal de negócios", $corpoHTML, $header_cliente);
+
+// Envio para o e-mail de leads
+$corpoHTML_leads = $corpoHTML . '<p><b> URL do Lead: </b>' .$url.' </p> <hr>';
+
+$header_leads    = $headers . "Return-Path: $emailatt_leads \r\n";
+$envio_leads     = mail($emailatt_leads,"[Lead] Brainstorm - Escoal de negócios", $corpoHTML_leads, $header_leads); 
+
+
+if($envio_cliente && $envio_leads) {
+  echo "<script>location.href='sucesso.html'</script>"; // Página que será redirecionada
+}
+
+?>
